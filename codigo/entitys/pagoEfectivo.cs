17b76c4878c36997ed58d59a3b588217f184c65b
Damian <damianpalacios570@gmail.com>
2026-06-08 14:37:04 -0300

    cree clase hija pago efectivo

---
+++ codigo/entitys/pagoEfectivo.cs
@@ -0,0 +1,15 @@
+using System;
+
+class PagoEfectivo : Pago
+{
+    public PagoEfectivo(string titular, double monto)
+        : base(titular, monto)
+    {
+
+    }
+
+    public override double CalcularComision()
+    {
+        return 0;
+    }
+}
