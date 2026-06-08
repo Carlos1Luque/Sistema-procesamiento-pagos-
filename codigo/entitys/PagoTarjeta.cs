b7d2a975025b1e5f9486fc047f52f0169a3ec8e9
Charlie <cmartinheredial22@gmail.com>
2026-06-08 19:49:31 -0300

    Se completo la clase (PagoTarjeta)

---
+++ codigo/entitys/PagoTarjeta.cs
@@ -0,0 +1,12 @@
+public class PagoTarjeta : Pago
+{
+    public PagoTarjeta(string titular, double monto)
+        : base(titular, monto)
+    {
+
+    }
+    public override double CalcularComision()
+    {
+        return Monto * 0.02;
+    }
+}
